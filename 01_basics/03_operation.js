3db79e2102988611e2e8c118599dbb0d7267bda0
Abhishek Singh <140716444+abhisheksingh-ai@users.noreply.github.com>
2024-03-10 07:39:41 +0000

    operatios

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,19 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2**2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(typeof console.log("1"+10))
+
+console.log(5+1+"8")
+console.log("8"+1+5)
+console.log(1+"8"+5)
+console.log(1+(2+1+"8"))
