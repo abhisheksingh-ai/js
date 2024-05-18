dd0dde02f4fd1ec7455dfd3089ffb9437b3b9d27
Abhishek Singh <140716444+abhisheksingh-ai@users.noreply.github.com>
2024-05-18 04:41:49 +0000

    Object me function bhia daal sakta hu

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,24 @@
+//const mySym = Symbol("key1") 
+
+const student = { 
+    Name : "Abhishek" ,
+    College : "IIT Kharagpur",
+    greeting2 : function(){console.log(`bhai aapka name, ${this.Name}hai`)},
+    Year : 2
+};
+// console.log(student.Year);
+// console.log(student.Name);
+// console.log(student.College);
+
+// console.log(student.mySym);
+// console.log(typeof student.mySym);
+// console.log(typeof mySym);
+
+student.greeting = function(){
+    console.log(`Hello Student, ${this.Name}. Your college name is ${this.College}`)
+}
+
+// console.log(student.greeting());
+
+// console.log(student)
+console.log(student.greeting2());
